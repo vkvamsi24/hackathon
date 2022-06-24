4291871c4c7df950844b0d6b075f33bbda64467b
Vamsi Krishna Thutupalli <t-vthutupall@microsoft.com>
2022-06-24 12:42:41 +0530

    added data

---
+++ src/data.js
@@ -0,0 +1,124 @@
+const output = [
+  {
+    callsign: "UAE866",
+    tsat: "2022-06-22 4:59:35",
+  },
+  {
+    callsign: "UAE835",
+    tsat: "2022-06-22 4:59:35",
+  },
+  {
+    callsign: "SWR243",
+    tsat: "2022-06-22 5:02:35",
+  },
+  {
+    callsign: "AXB344",
+    tsat: "2022-06-22 5:02:35",
+  },
+  {
+    callsign: "UAE823",
+    tsat: "2022-06-22 5:04:35",
+  },
+  {
+    callsign: "FDB251",
+    tsat: "2022-06-22 5:06:35",
+  },
+  {
+    callsign: "UAE231",
+    tsat: "2022-06-22 5:05:35",
+  },
+  {
+    callsign: "UAE701",
+    tsat: "2022-06-22 5:09:35",
+  },
+  {
+    callsign: "BAW106",
+    tsat: "2022-06-22 5:09:35",
+  },
+  {
+    callsign: "UAE582",
+    tsat: "2022-06-22 5:11:35",
+  },
+  {
+    callsign: "UAE430",
+    tsat: "2022-06-22 5:16:35",
+  },
+  {
+    callsign: "UAE705",
+    tsat: "2022-06-22 5:16:35",
+  },
+  {
+    callsign: "AHY016",
+    tsat: "2022-06-22 5:16:35",
+  },
+  {
+    callsign: "UAE8ER",
+    tsat: "2022-06-22 5:20:35",
+  },
+  {
+    callsign: "UAE604",
+    tsat: "2022-06-22 5:21:35",
+  },
+  {
+    callsign: "UAE650",
+    tsat: "2022-06-22 6:23:35",
+  },
+  {
+    callsign: "THY5GC",
+    tsat: "2022-06-22 6:24:35",
+  },
+  {
+    callsign: "AFL525",
+    tsat: "2022-06-22 6:24:35",
+  },
+  {
+    callsign: "UAE7MY",
+    tsat: "2022-06-22 6:27:35",
+  },
+  {
+    callsign: "UAE656",
+    tsat: "2022-06-22 6:26:35",
+  },
+  {
+    callsign: "UAE530",
+    tsat: "2022-06-22 6:29:35",
+  },
+  {
+    callsign: "UAE338",
+    tsat: "2022-06-22 6:37:35",
+  },
+  {
+    callsign: "UAE221",
+    tsat: "2022-06-22 6:34:35",
+  },
+  {
+    callsign: "UAE544",
+    tsat: "2022-06-22 6:40:35",
+  },
+  {
+    callsign: "UAE9KC",
+    tsat: "2022-06-22 6:37:35",
+  },
+  {
+    callsign: "UAE223",
+    tsat: "2022-06-22 6:39:35",
+  },
+  {
+    callsign: "UAE1KM",
+    tsat: "2022-06-22 6:39:35",
+  },
+  {
+    callsign: "UAE318",
+    tsat: "2022-06-22 6:39:35",
+  },
+  {
+    callsign: "AFG402",
+    tsat: "2022-06-22 6:44:35",
+  },
+  {
+    callsign: "SEJ018",
+    tsat: "2022-06-22 6:47:35",
+  },
+];
+
+export default output;
